f11d6d8a521a43768d5d2faffa9ae21ff2357d0c
Farsit-007 <public>
2025-01-22 14:02:28 +0600

    Updated

--- src/app/middleware/validateRequest.ts
+++ src/app/middleware/validateRequest.ts
@@ -7,7 +7,6 @@ export const validation = (schema: AnyZodObject) => {
         async (req: Request, res: Response, next: NextFunction) => {
             await schema.parseAsync({
                 body: req.body,
-                cookies: req.cookies,
             })
             next()
         }
