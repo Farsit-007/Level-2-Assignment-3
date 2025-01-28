310c9905228811e0818c62ffb399bd4303edfdc1
Farsit-007 <public>
2025-01-28 17:48:13 +0600

    Updated

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import { globalErrorHandler } from './app/middleware/globalErrorHandler'
 import { notFound } from './app/middleware/notFound'
 import router from './app/routes'
 import cookieParser from 'cookie-parser'
-import config from './app/config'
 const app: Application = express()
 
 app.use(express.json())
@@ -13,9 +12,6 @@ app.use(cors({ origin: ['http://localhost:5173'] }))
 
 app.use('/api', router)
 
-app.use(globalErrorHandler)
-app.use(notFound)
-
 app.get('/', (req, res) => {
     res.send('Assignment Running')
 })
@@ -23,8 +19,6 @@ app.get('/api', (req, res) => {
     res.send('Api Routes')
 })
 
-app.listen(config.port, () => {
-    console.log(`Project is Running on ${config.port}`)
-})
-
+app.use(globalErrorHandler)
+app.use(notFound)
 export default app
